e6dbd03b4ab5766f2f8455dea723c3a5871f2dc3
seif <seenaseif@gmail.com>
2024-02-13 20:32:08 +0200

    just laoder started

---
+++ src/components/Loader.jsx
@@ -0,0 +1,46 @@
+import gsap from "gsap";
+import { TextPlugin } from "gsap/all";
+import React, { useEffect, useRef } from "react";
+
+const Loader = () => {
+  gsap.registerPlugin(TextPlugin);
+  const textWrapper = useRef(null);
+  const text = useRef(null);
+  useEffect(() => {
+    const tl = gsap.timeline({
+      defaults: {
+        duration: 1,
+        ease: "power3",
+        stagger: 0.2,
+      },
+    });
+    tl.to(text.current, {
+      y: 0,
+      fontFamily: "media",
+    });
+    tl.to(text.current, {
+      text: "Hola",
+      y: 0,
+      fontFamily: "media",
+    });
+    tl.to(text.current, { text: "Bonjour", fontFamily: "media" });
+    tl.to(text.current, { text: "こんにちは", fontWeight: "bold" });
+    tl.to(text.current, { text: "नमस्ते", fontFamily: "media" });
+    tl.to(text.current, { text: "السلام عليكم", fontFamily: "kaman" });
+    tl.to(text.current, { y: 50, fontFamily: "kaman" });
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="h-11 flex-col flex justify-center items-center overflow-hidden w-fit text-center ">
+        <div className=" " ref={textWrapper}>
+          <h1 className=" p-2 text-4xl -translate-y-12" ref={text}>
+            wow
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
